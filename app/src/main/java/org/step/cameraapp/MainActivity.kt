bca78c9e5e7d594873d6a8478b0dabfd01ae13ad
Kamila <zhakupova.kamila@gmail.com>
2021-03-02 20:41:32 +0600

    Initial commit

---
+++ app/src/main/java/org/step/cameraapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package org.step.cameraapp
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    private val cameraRequest = 1888
+    lateinit var photo_imageview: ImageView
+    private var camera_button: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermissions()
+        initializeViews()
+        initializeListeners()
+    }
+
+    private fun initializeViews() {
+        camera_button = findViewById(R.id.button_activity_main_camera)
+        photo_imageview = findViewById(R.id.imageview_activity_main_photo)
+    }
+
+
+    private fun initializeListeners() {
+        camera_button?.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED
+        )
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                cameraRequest
+            )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            photo_imageview.setImageBitmap(photo)
+        }
+    }
+}
